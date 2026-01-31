87b73669f115135cfe2bffe7f999e17c0ea0c7df
hunsri <richard.hunstock@gmx.de>
2026-01-31 21:12:48 +0100

    adds proper websocket for asset download

--- main.py
+++ main.py
@@ -22,8 +22,8 @@ async def websocket(websocket: WebSocket):
     await websocket.accept()
     await websocket.send_json({"msg": "Hello WebSocket"})
 
-@app.get("/ws/assets/categories/{category_name}/{asset_name}/download")
-async def download_asset(category_name: str, asset_name: str):
+@app.get("/assets/categories/{category_name}/{asset_name}/download")
+async def download_asset_http(category_name: str, asset_name: str):
     try:
         asset_path = manager.get_asset_file_path(category_name, asset_name)
         with open(asset_path, "rb") as file:
@@ -31,6 +31,19 @@ async def download_asset(category_name: str, asset_name: str):
         return Response(content=content, media_type="application/octet-stream", headers={"Content-Disposition": f"attachment; filename={asset_name}.zip"})
     except ValueError as e:
         raise HTTPException(status_code=404, detail=str(e))
+
+#TODO stream in chunks for large files
+@app.websocket("/ws/assets/categories/{category_name}/{asset_name}/download")
+async def download_asset(websocket: WebSocket, category_name: str, asset_name: str):
+    await websocket.accept()
+    try:
+        asset_path = manager.get_asset_file_path(category_name, asset_name)
+        with open(asset_path, "rb") as file:
+            content = file.read()
+
+        await websocket.send_bytes(content)
+    except Exception as e:
+        await websocket.send_text(f"Error: {str(e)}")
 
 @app.get("/assets/categories/{category_name}/{asset_name}/asset_info_file")
 async def get_asset_info_file(category_name: str, asset_name: str):
